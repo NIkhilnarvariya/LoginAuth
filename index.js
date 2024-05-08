e4877333d6e99c60860c2623f5fafb16419de99e
NIkhilnarvariya <narvariyanikhil@gmail.com>
2024-05-08 11:23:57 +0530

    second commit

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-const express=require("express")
-const mongoose =require("mongoose")
-const cors=require("cors")
-const EmployeeModel=require('./model/Employee')
-const app=express()
-app.use(express.json())
-app.use(cors())
-mongoose.connect("mongodb://127.0.0.1:27017/employewe")
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const EmployeeModel = require('./model/Employee');
+const app = express();
 
-app.post("/login",(req,res)=>{
-    const {email,password}=req.body;
-    EmployeeModel.findOne({email:email})
-    .then(user=>{
-        if(user){
-            if(user.password==password){
-                res.json("SUCESSSS")
-            }else{
-                res.json("INCOREECT PASSWORD")
-            }}else{
-                res.json("NOT EXIST")
-            
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("Failed to connect to MongoDB", err));
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    EmployeeModel.findOne({ email: email })
+    .then(user => {
+        if (user) {
+            if (user.password == password) {
+                res.json("SUCCESS");
+            } else {
+                res.json("INCORRECT PASSWORD");
+            }
+        } else {
+            res.json("NOT EXIST");
         }
     })
-})
-app.post('/register',(req,res)=>{
+    .catch(err => res.json(err));
+});
+
+app.post('/register', (req, res) => {
      EmployeeModel.create(req.body)
-     .then(employee=>res.json(employee))
-     .catch(err=>res.json(err))
-})
-app.listen(3001,()=>{
-    console.log("SERVICE UP")
-})
+     .then(employee => res.json(employee))
+     .catch(err => res.json(err));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Service running on port ${PORT}`);
+});
